671337c1583f530353ab0eeff41186194800ebb5
Robert Stewart <robertstewart@Roberts-Air.lan>
2025-10-04 00:41:41 -0400

    Fix SQL: use CTEs for zhvi/zori, avoid Date alias bug

--- transform/build_zip_metrics.sql
+++ transform/build_zip_metrics.sql
@@ -1,7 +1,22 @@
-CREATE OR REPLACE TABLE zip_metrics AS
-SELECT RegionName AS zcta, Date AS date, Value AS zhvi FROM zhvi_raw;
-CREATE OR REPLACE TABLE zip_rent AS
-SELECT RegionName AS zcta, Date AS date, Value AS zori FROM zori_raw;
+WITH zhvi_clean AS (
+  SELECT 
+    RegionName AS zcta,
+    "Date" AS date,
+    Value AS zhvi
+  FROM zhvi_raw
+),
+zori_clean AS (
+  SELECT 
+    RegionName AS zcta,
+    "Date" AS date,
+    Value AS zori
+  FROM zori_raw
+)
 CREATE OR REPLACE TABLE zip_metrics_monthly AS
-SELECT m.zcta, m.date, m.zhvi, r.zori FROM zip_metrics m
-LEFT JOIN zip_rent r USING (zcta, date);
+SELECT 
+  m.zcta,
+  m.date,
+  m.zhvi,
+  r.zori
+FROM zhvi_clean m
+LEFT JOIN zori_clean r USING (zcta, date);
